bb826a0404bda3eada98fc3718cb7a24bb19519d
LongfeiH <2069860997@qq.com>
2024-02-18 17:13:04 +0800

    add workflows

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,53 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const nav = (() => {
-    const nav = document.querySelector(".main-navigation");
-    const navOpen = document.querySelector(".mobile-nav-open");
-    const navClose = document.querySelector(".mobile-nav-close");
-    navOpen.addEventListener("click", () => nav.classList.add("active"));
-    navClose.addEventListener("click", () => nav.classList.remove("active"));
-  })();
+  // const nav = (() => {
+  //   const nav = document.querySelector(".main-navigation");
+  //   const navOpen = document.querySelector(".mobile-nav-open");
+  //   const navClose = document.querySelector(".mobile-nav-close");
+  //   navOpen.addEventListener("click", () => nav.classList.add("active"));
+  //   navClose.addEventListener("click", () => nav.classList.remove("active"));
+  // })();
 
   const search = (() => {
     const searchResult = (result) =>
-      `<div class="search-result"><a href="${result.url}">${result.name}</a></div>`;
-    const search = document.querySelector("#search-form");
-    const searchResults = document.querySelector("#search-results");
-
-    search.addEventListener("submit", (e) => {
+      `<tr>
+    <td><a href="${result.url}">${result.name}</a></td>
+    <td>${result.language}</td>
+    <td>${result.category}</td>
+    <td>${result.formation}</td>
+    <td>${result.spinneret}</td>
+    <td>${result.academic_year}</td>
+    <td>${result.type_of_training}</td>
+    <td>${result.semester}</td>
+    </tr>`;
+    const searchForm = document.querySelector("#search-form");
+    const submit = document.querySelector("#submit")
+    const tbody = document.querySelector("#search-courses");
+    submit.addEventListener("click", (e) => {
       e.preventDefault();
-      searchResults.innerHTML = "";
-      const output = document.createElement("div");
-      const data = new FormData(e.target);
-      const query = data.get("query");
+      tbody.innerHTML = "";
+      const inputs = searchForm .querySelectorAll("input, select");
+      const formData = {};
+      inputs.forEach((input) => {
+        formData[input.name] = input.value;
+      });
+      console.log('f',formData);
       const pages = JSON.parse(SITE_DATA.pages);
+      console.log(pages);
       const results =
         Array.from(pages)?.filter((page) =>
-          page.name.toLowerCase().includes(query.toLowerCase())
+          page.name.toLowerCase().includes(formData.query.toLowerCase())&&
+          page.academic_year === formData.academic_year&&
+          page.category === formData.category&&
+          page.formation === formData.formation&&
+          page.spinneret === formData.spinneret&&
+          page.language === formData.language&&
+          page.type_of_training === formData.type_of_training&&
+          page.semester === formData.semester
         ) || [];
-      if (results.length > 0 && query.length > 0) {
+      console.log('results', results);
+      if (results.length > 0) {
         const markup = results.map((result) => searchResult(result));
-        output.innerHTML = markup.join("");
-        searchResults.appendChild(output);
-      } else {
-        output.innerHTML = `<p class="empty-notice">No results found for ${query}</p>`;
-        searchResults.appendChild(output);
+        tbody.innerHTML = markup.join("");
       }
     });
   })();
